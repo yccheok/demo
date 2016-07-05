ee100b8d0f137d621392166cacdfae770578b341
Yan Cheng Cheok <yccheok@yahoo.com>
2016-07-06 01:29:42 +0800

    ...

--- app/src/main/java/com/example/yccheok/demo/MainFragment.java
+++ app/src/main/java/com/example/yccheok/demo/MainFragment.java
@@ -2,6 +2,9 @@ package com.example.yccheok.demo;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,46 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ViewPager viewPager = (ViewPager)view.findViewById(R.id.view_pager);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        viewPager.setAdapter(sectionsPagerAdapter);
+
         return view;
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return NestedFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/example/yccheok/demo/NestedFragment.java
@@ -0,0 +1,44 @@
+package com.example.yccheok.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by yccheok on 6/7/2016.
+ */
+public class NestedFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public NestedFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static NestedFragment newInstance(int sectionNumber) {
+        NestedFragment fragment = new NestedFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_nested, container, false);
+        TextView textView = (TextView) view.findViewById(R.id.text_view);
+        textView.setText("Fragment " + getArguments().getInt(ARG_SECTION_NUMBER));
+        return view;
+    }
+
+}
